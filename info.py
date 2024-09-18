011a480dfc43e076bd0247b2cd17bdeb3be7dc21
Strelizia-007 <143823798+Strelizia-007@users.noreply.github.com>
2024-09-18 17:49:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6804220356:AAG675PNd7lSY2B8BcAZdpnwenKgR0W
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
-NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/15382eeff7d2f95d59d6f.jpg")
+NOR_IMG = environ.get('NOR_IMG', "https://envs.sh/qhm.jpg")
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/f83017d890b1b692a673a.jpg https://te.legra.ph/file/72602fb1f87727dde571b.jpg https://te.legra.ph/file/e37fbea2190470aaf4bc1.jpg https://te.legra.ph/file/9f4f70db519f1bdbcb27b.jpg https://te.legra.ph/file/72d74f632a2317f21136b.jpg https://te.legra.ph/file/018e5893fdaabe0f1cef4.jpg')).split()
 SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/314928baed9a9c277072f.jpg")
 
